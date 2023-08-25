ec2c2a33dd72f0b102bcc9ed49e10f09e896683d
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-25 17:22:23 +0400

    Finish body multiple parameters and Update requirements

--- 01-tutorial/07-body-multiple-parameters/main.py
+++ 01-tutorial/07-body-multiple-parameters/main.py
@@ -1,7 +1,7 @@
 from typing import Union, Annotated
 
 import uvicorn
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Body, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,17 +14,32 @@ class Item(BaseModel):
     tax: Union[float, None] = None
 
 
-@app.put("/items/{item_id}")
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+@app.put("/items_1/{item_id}")
 async def update_item(
+        *,
         item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
         q: Union[str, None] = None,
+        w: Annotated[int, Query(gt=1)],
         item: Union[Item, None] = None,
+        user: User,
+        importance: Annotated[int, Body(gt=4)]
 ):
     results = {"item_id": item_id}
     if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
+        results.update({"q": q, "w": w})
+        # if item:
+        results.update({"item": item, "user": user, "importance": importance})
+    return results
+
+
+@app.put("/items_2/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
     return results
 
 

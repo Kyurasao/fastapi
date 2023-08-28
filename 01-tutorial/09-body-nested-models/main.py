fa1f791fca225316e58b91a87c6965c5bb4761d4
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-28 13:10:18 +0400

    Question

--- 01-tutorial/09-body-nested-models/main.py
+++ 01-tutorial/09-body-nested-models/main.py
@@ -1,10 +1,16 @@
 from typing import Union
 
 import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url_1: str
+    url_2: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
@@ -12,13 +18,27 @@ class Item(BaseModel):
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: list = []
+    tags_1: list = []
+    tags_2: set[str] = set()
+    image: Union[list[Image], None] = None
+
+
+class Offer(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    items: list[Item]
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item): #  = Body(emded=True)
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
 
 
 if __name__ == "__main__":

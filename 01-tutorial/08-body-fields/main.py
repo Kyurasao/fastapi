9b295cda5045f2a389b505e948ba9ee9c8829f2d
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-28 09:36:25 +0400

    Finish body fields and update requirements

--- 01-tutorial/08-body-fields/main.py
+++ 01-tutorial/08-body-fields/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import uvicorn
 from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
 
@@ -19,3 +20,7 @@ class Item(BaseModel):
 async def update_item(item_id: int, item: Item = Body(embed=True)):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80)

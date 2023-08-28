8856e1ccecb00b6ef93ed57045df85ecdca97aa6
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-28 09:43:32 +0400

    Work on body - nested models and update requirements

---
+++ 01-tutorial/09-body-nested-models/main.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: list = []
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
